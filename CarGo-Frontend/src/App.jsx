28237129b1b16290082cc295c5b2ef0e262f3692
PasiSandeepa <pasisandeepa456@gmail.com>
2025-12-19 20:22:43 +0530

    App.jsx Implement Changes

--- CarGo-Frontend/src/App.jsx
+++ CarGo-Frontend/src/App.jsx
@@ -1,16 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Vehicle from './pages/Vehicle';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
-  )
+    <div className="App" style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <Navbar notifications={[]} />
+
+
+      <div style={{ marginTop: '70px', flex: 1 }}>
+        <Routes>
+
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+
+          <Route
+            path="/vehicles"
+            element={
+              <div className="container">
+                <Vehicle />
+              </div>
+            }
+          />
+        </Routes>
+      </div>
+
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
